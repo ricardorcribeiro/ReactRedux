f3e35196b339d66805bff45d748421c87e058a2f
Ricardo Ribeiro <ricardorcriberio@gmail.com>
2019-07-25 21:09:40 -0300

    primeiro componente

---
+++ exercicios-react/src/Componentes/PrimeiroComponente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => 
+    <h1>Primeiro componente!</h1>
+
+
+// function primeiro(){
+//     return <h1>Primeiro componente!</h1>
+// }
+
+// export default primeiro
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,5 +1,18 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import PrimeiroComponente from './Componentes/PrimeiroComponente'
 
 const element = document.getElementById('root')
-ReactDOM.render(<h1>ola react</h1>, element)
+ ReactDOM.render(
+ 
+    <div>
+        <PrimeiroComponente/>
+    </div>
+ 
+    , element)
+
+
+// ReactDOM.render(<h1>ola react</h1>, element)
+
+
+
