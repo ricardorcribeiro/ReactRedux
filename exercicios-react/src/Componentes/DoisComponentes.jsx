31fe5ba607193334ecd9d9541a285342e880acf5
Ricardo Ribeiro <ricardorcriberio@gmail.com>
2019-07-25 21:50:33 -0300

    dois componentes em um arqueivo

---
+++ exercicios-react/src/Componentes/DoisComponentes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompA = props => 
+    <h1>primeiro diz:{props.valor}</h1>
+
+const CompB = props => 
+    <h1>segundo diz:{props.valor}</h1>
+
+export  {CompA, CompB}
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import PrimeiroComponente from './Componentes/PrimeiroComponente'
+import { CompA, CompB as B } from './Componentes/DoisComponentes'
+
 
 const element = document.getElementById('root')
  ReactDOM.render(
  
     <div>
         <PrimeiroComponente valor="Bom dia!" operacao={2**8}/>
+
+        <CompA valor={Math.random()}/>
+        <B valor="bananinha 123"/>
     </div>
  
     , element)
