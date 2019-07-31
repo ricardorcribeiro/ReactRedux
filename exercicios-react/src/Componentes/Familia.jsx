711189549361e7bb0a53328e0bb4134ae9ab5d72
Ricardo Ribeiro <ricardorcriberio@gmail.com>
2019-07-30 23:24:09 -0300

    colocando componente dentro do corpo de outro componente

---
+++ exercicios-react/src/Componentes/Familia.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => 
+<div>
+    {/* {children pega todos os elementos internos passados no dentro ta teg } */}
+    {props.children}
+</div>
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -4,6 +4,8 @@ import PrimeiroComponente from './Componentes/PrimeiroComponente'
 import { CompA, CompB as B } from './Componentes/DoisComponentes'
 import Multiplo from './Componentes/MultiElementos'
 import FamiliaRibeiro  from './Componentes/FamiliaRibeiro'
+import Familia from './Componentes/Familia'
+import Membors from './Componentes/Membro'
 
 const element = document.getElementById('root')
  ReactDOM.render(
@@ -17,6 +19,15 @@ const element = document.getElementById('root')
         <Multiplo/>
 
         <FamiliaRibeiro/>
+
+        <Familia>
+            <Membors nome="ricardo" sobrenome="ribeiro" />
+            <Membors nome="rafaela" sobrenome="ribeiro" />
+            <Membors nome="mariza" sobrenome="ribeiro" />
+            <Membors nome="zeus" sobrenome="ribeiro" />
+            <Membors nome="gaia" sobrenome="ribeiro" />
+
+        </Familia>
     </div>
  
     , element)
