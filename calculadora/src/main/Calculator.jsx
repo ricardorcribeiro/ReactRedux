73f0b944691ac81eaa9f360f56c9c41f1c8f00cc
Ricardo Ribeiro <ricardorcriberio@gmail.com>
2019-08-10 17:26:49 -0300

    termino calculadora

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -13,7 +13,7 @@ const initialState = {
 
 export default class Calculator extends Component{
 
-    state = { ... initialState }//variavel "state" e uma variavel que e do react
+    state = { ... initialState }
     clearMemory(){
         this.setState({... initialState})
     }
@@ -24,6 +24,23 @@ export default class Calculator extends Component{
         else{
             const equals = operation === '='
             const currentOperation = this.state.operation
+
+            const values = [... this.state.values]
+
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch (e) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] =0
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDiplay: !equals,
+                values
+            })
         }
     }
 
