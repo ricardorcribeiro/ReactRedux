81c24f805934fe2b1d9544c952df5aaebe7eb87a
Ricardo Ribeiro <ricardorcriberio@gmail.com>
2019-08-20 23:17:30 -0300

    adcao dos cors e comeco criacao componentes front

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -25,6 +25,18 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+        {
+            options.AddPolicy("minhaEspecificacoesDeOrigem",
+            builder =>
+            {
+                builder.WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+            });
+        });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import './App.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import Todo from './todo/todo'
+import About from './about/about'
 
-function App() {
-  return (
-    <div className="conteiner">
-    </div>
-  );
-}
+export default props =>
+<div className='container'>
+  <Todo/>
+  <About/> 
+</div>
 
-export default App;
+
+// function App() {
+//   return (
+//     <div className="conteiner">
+//     </div>
+//   );
+// }
+
+// export default App;
